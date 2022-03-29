5e9d0ef6fd3c9b9b08f9b426030ff009984769e8
LMFront-end <perfilgitlinamaria@gmail.com>
2022-03-28 23:17:32 -0500

    se crea DoctorRepository

--- src/main/java/com/example/appcitasmedicas/domain/repositories/DiseaseRepository.java
+++ src/main/java/com/example/appcitasmedicas/domain/repositories/DiseaseRepository.java
@@ -2,6 +2,8 @@ package com.example.appcitasmedicas.domain.repositories;
 
 import com.example.appcitasmedicas.domain.collections.Disease;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface DiseaseRepository extends ReactiveMongoRepository<Disease, String> {
 }
--- src/main/java/com/example/appcitasmedicas/domain/repositories/DoctorRepository.java
+++ src/main/java/com/example/appcitasmedicas/domain/repositories/DoctorRepository.java
@@ -1,4 +1,9 @@
 package com.example.appcitasmedicas.domain.repositories;
 
-public interface DoctorRepository {
+import com.example.appcitasmedicas.domain.collections.Doctor;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DoctorRepository extends ReactiveMongoRepository<Doctor, String> {
 }
