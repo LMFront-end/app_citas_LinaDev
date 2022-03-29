68060b10899fc96bfc8af563960eff1c7932df7c
LMFront-end <perfilgitlinamaria@gmail.com>
2022-03-28 23:15:19 -0500

    se crea AppointmentRepository

--- src/main/java/com/example/appcitasmedicas/domain/repositories/AppointmentRepository.java
+++ src/main/java/com/example/appcitasmedicas/domain/repositories/AppointmentRepository.java
@@ -1,4 +1,9 @@
 package com.example.appcitasmedicas.domain.repositories;
 
-public interface AppointmentRepository {
+import com.example.appcitasmedicas.domain.collections.Appointment;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AppointmentRepository extends ReactiveMongoRepository<Appointment, String> {
 }
