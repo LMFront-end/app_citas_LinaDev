d3c44d6d33c784d2b8570a1275c9b2fe7a080b83
LMFront-end <perfilgitlinamaria@gmail.com>
2022-03-28 21:35:37 -0500

    Se configura conexion a MongoDB

---
+++ src/main/java/com/example/appcitasmedicas/AppCitasMedicasApplication.java
@@ -0,0 +1,13 @@
+package com.example.appcitasmedicas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppCitasMedicasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppCitasMedicasApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/appcitasmedicas/AppCitasMedicasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.appcitasmedicas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppCitasMedicasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
