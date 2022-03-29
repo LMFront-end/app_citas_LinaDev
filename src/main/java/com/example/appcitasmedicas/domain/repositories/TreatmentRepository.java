b65cf5c07d35372ab9000fcacfdf97f6f2a0ce67
LMFront-end <perfilgitlinamaria@gmail.com>
2022-03-28 23:19:53 -0500

    se crea TreatmentRepository

--- src/main/java/com/example/appcitasmedicas/domain/repositories/TreatmentRepository.java
+++ src/main/java/com/example/appcitasmedicas/domain/repositories/TreatmentRepository.java
@@ -1,4 +1,9 @@
 package com.example.appcitasmedicas.domain.repositories;
 
-public interface TreatmentRepository {
+import com.example.appcitasmedicas.domain.collections.Treatment;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TreatmentRepository extends ReactiveMongoRepository<Treatment, String> {
 }
