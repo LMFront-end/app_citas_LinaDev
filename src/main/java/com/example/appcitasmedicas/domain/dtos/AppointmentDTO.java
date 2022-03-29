f0ed24ef426969b0982050fa736dc202693d41db
LMFront-end <perfilgitlinamaria@gmail.com>
2022-03-28 22:37:36 -0500

    se completan datos en dto

--- src/main/java/com/example/appcitasmedicas/domain/dtos/AppointmentDTO.java
+++ src/main/java/com/example/appcitasmedicas/domain/dtos/AppointmentDTO.java
@@ -1,4 +1,16 @@
 package com.example.appcitasmedicas.domain.dtos;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class AppointmentDTO {
+
+    private String id;
+    private String patientId;
+    private String doctorId;
+    private String disease;
 }
--- src/main/java/com/example/appcitasmedicas/domain/dtos/PatientDTO.java
+++ src/main/java/com/example/appcitasmedicas/domain/dtos/PatientDTO.java
@@ -1,4 +1,15 @@
 package com.example.appcitasmedicas.domain.dtos;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class PatientDTO {
+
+    private String id;
+    private String firstName;
+    private String lastName;
 }
