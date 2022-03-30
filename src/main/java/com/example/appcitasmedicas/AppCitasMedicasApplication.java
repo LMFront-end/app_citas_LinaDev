adec1817ef69855843bc1d239552737acd77130e
LMFront-end <perfilgitlinamaria@gmail.com>
2022-03-30 01:25:00 -0500

    no sirvio el swagger

--- src/main/java/com/example/appcitasmedicas/AppCitasMedicasApplication.java
+++ src/main/java/com/example/appcitasmedicas/AppCitasMedicasApplication.java
@@ -3,6 +3,7 @@ package com.example.appcitasmedicas;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -11,18 +12,7 @@ import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-@EnableReactiveMongoRepositories
-@EnableSwagger2
 public class AppCitasMedicasApplication {
-
-    @Bean
-    public Docket citasApi(){
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.any())
-                .build();
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(AppCitasMedicasApplication.class, args);
