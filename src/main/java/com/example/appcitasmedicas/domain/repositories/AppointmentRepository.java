17c888bdcd18451f658fa169b9b831a126757fd7
LMFront-end <perfilgitlinamaria@gmail.com>
2022-03-30 00:21:09 -0500

    se crea conexion a swagger

--- src/main/java/com/example/appcitasmedicas/domain/repositories/AppointmentRepository.java
+++ src/main/java/com/example/appcitasmedicas/domain/repositories/AppointmentRepository.java
@@ -5,8 +5,12 @@ import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Flux;
 
+import java.time.LocalDate;
+
 @Repository
 public interface AppointmentRepository extends ReactiveMongoRepository<Appointment, String> {
     Flux<Appointment> findAllByDoctorId(String id);
     Flux<Appointment> findAllByPatientId(String id);
+    Flux<Appointment> findAllByAppointmentDetails_Date(LocalDate localDate);
+
 }
