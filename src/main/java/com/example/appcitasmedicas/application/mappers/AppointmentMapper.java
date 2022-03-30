d545407834620d8a21c254a33a78a1c75f80a3d2
LMFront-end <perfilgitlinamaria@gmail.com>
2022-03-29 22:58:44 -0500

    se crean appointmentMapper

---
+++ src/main/java/com/example/appcitasmedicas/application/mappers/AppointmentMapper.java
@@ -0,0 +1,23 @@
+package com.example.appcitasmedicas.application.mappers;
+
+import com.example.appcitasmedicas.domain.collections.Appointment;
+import com.example.appcitasmedicas.domain.dtos.AppointmentDTO;
+
+import java.util.function.Function;
+
+public class AppointmentMapper {
+
+    public Function<Appointment, AppointmentDTO> mapToDTO(){
+        return appointment -> new AppointmentDTO(appointment.getId(),
+                appointment.getPatientId(),
+                appointment.getDoctorId(),
+                appointment.getAppointmentDetails());
+    }
+
+    public Function<AppointmentDTO, Appointment> mapToCollection(){
+        return appointmentDTO -> new Appointment(appointmentDTO.getId(),
+                appointmentDTO.getPatientId(),
+                appointmentDTO.getDoctorId(),
+                appointmentDTO.getAppointmentDetails());
+    }
+}
