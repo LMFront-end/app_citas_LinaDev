64205df6934c39a0d9321bf3ed6512375f4a4c21
LMFront-end <perfilgitlinamaria@gmail.com>
2022-03-29 22:53:18 -0500

    se agrega anotacion en AppointmentDetails

--- src/main/java/com/example/appcitasmedicas/domain/valueObjects/AppointmentDetails.java
+++ src/main/java/com/example/appcitasmedicas/domain/valueObjects/AppointmentDetails.java
@@ -1,5 +1,6 @@
 package com.example.appcitasmedicas.domain.valueObjects;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
 import java.time.LocalDate;
@@ -7,6 +8,7 @@ import java.time.LocalDate;
 @Data
 public class AppointmentDetails {
 
+    @JsonFormat(pattern = "dd-MM-yyyy")
     private final LocalDate date;
     private final AppointmentStatus appointmentStatus;
     private final Triage triage;
